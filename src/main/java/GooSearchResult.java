572af4a235b80c3b44c8dc250af405c29de4e049
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2021-04-02 20:23:20 +0300

    added class GooSearchResult

---
+++ src/main/java/GooSearchResult.java
@@ -0,0 +1,26 @@
+class GooSearchResult {
+    private String url;
+
+    public GooSearchResult(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String parseDomain() {
+        String[] arrUrl = url.split("/");
+        return arrUrl[2];
+    }
+}
+
+class GooSearchResultTest {
+    public static void main(String[] args) {
+        //test.com
+        System.out.println(new GooSearchResult("https://test.com").parseDomain());
+
+        //apple.in.mars
+        System.out.println(new GooSearchResult("http://apple.in.mars").parseDomain());
+    }
+}
