09d2a9aad2db364cea5bc7e0a51b2f2445f43ff8
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2021-04-04 11:06:30 +0300

    fixed class WordFreqCounter

--- src/main/java/WordFreqCounter.java
+++ src/main/java/WordFreqCounter.java
@@ -4,7 +4,7 @@ class WordFreqCounter {
         String[] arrPhrase = phrase.toLowerCase().split(" ");
         float countWords = arrPhrase.length;
         for (String el : arrPhrase) {
-            if (el.equals(word)) {
+            if (el.equals(word.toLowerCase())) {
                 freq++;
             }
         }
@@ -18,8 +18,11 @@ class WordFreqCounterTest {
 
         //0.5
         System.out.println(freqCounter.countFreq("Hello Java", "java"));
-
         //0
         System.out.println(freqCounter.countFreq("Hello World", "java"));
+        //0.1428571492433548
+        System.out.println(freqCounter.countFreq("syhemidoxix di sava Otrujasaby paybnudou ihdapoqaypb hiut", "Otrujasaby"));
+        //0.2222222238779068
+        System.out.println(freqCounter.countFreq("wodeciwyeqq nuacrezy Noobduxu xowehe havacibafeg Noobduxu pe ibaxpoq avwep", "Noobduxu"));
     }
 }
